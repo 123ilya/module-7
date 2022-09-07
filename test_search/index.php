ffee6f93bcae139490fbfe97115c398b6695f119
Илья Соболев <123ilya@gmail.com>
2022-09-07 21:58:01 +1000

    first commit

---
+++ test_search/index.php
@@ -0,0 +1,19 @@
+<?php
+
+//$dir = 'C:\Users\sobolev_ia\Desktop';
+//$files = scandir($dir);
+//var_dump(is_dir($dir));
+$searchRoot = '//opt/lampp/htdocs/Welcome/7/module-7/test_search';
+$searchName = 'test.html';
+$searchResult = [];
+
+function search($dir)
+{
+    foreach (scandir($dir) as $item) {
+        if ($item !== '.' && $item !== '..') {
+            echo $item . PHP_EOL;
+        }
+    }
+}
+
+search($searchRoot);

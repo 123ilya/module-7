cfca2d677ece1662ca86ad2060c2102b2b9f0225
Илья Соболев <123ilya@gmail.com>
2022-09-08 20:11:29 +1000

    исправил условие

--- index.php
+++ index.php
@@ -13,7 +13,9 @@ function search($dir)
     foreach (scandir($dir) as $item) {
         if ($item !== '.' && $item !== '..' && is_dir($dir.'/'.$item)) {
             echo 'folder '. $item.PHP_EOL;
-        } else echo 'file '.$item.PHP_EOL;
+        } elseif ($item !== '.' && $item !== '..' && !is_dir($dir.'/'.$item)){
+            echo 'file '. $item.PHP_EOL;
+        }
 
     }
 }

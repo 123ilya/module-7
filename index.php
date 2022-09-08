b13fdd540a0f31f925acaa7e798ff14e45e6a97e
Илья Соболев <123ilya@gmail.com>
2022-09-08 20:08:36 +1000

    Обход элементов дирректории работает

--- index.php
+++ index.php
@@ -3,17 +3,18 @@
 //$dir = 'C:\Users\sobolev_ia\Desktop';
 //$files = scandir($dir);
 //var_dump(is_dir($dir));
-//$searchRoot = '//opt/lampp/htdocs/Welcome/7/module-7/test_search';
-$searchRoot = 'C:\Users\sobolev_ia\Desktop\PHP study\module-7\test_search';
+$searchRoot = '/opt/lampp/htdocs/Welcome/module-7/test_search';
+//$searchRoot = 'C:\Users\sobolev_ia\Desktop\PHP study\module-7\test_search';
 $searchName = 'test.html';
 $searchResult = [];
 
 function search($dir)
 {
     foreach (scandir($dir) as $item) {
-        if ($item !== '.' && $item !== '..') {
-            echo $item . PHP_EOL;
-        }
+        if ($item !== '.' && $item !== '..' && is_dir($dir.'/'.$item)) {
+            echo 'folder '. $item.PHP_EOL;
+        } else echo 'file '.$item.PHP_EOL;
+
     }
 }
 

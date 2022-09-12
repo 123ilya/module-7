b1a691670d1ee0b1f9f7be0ed55596f918054e85
Илья Соболев <123ilya@gmail.com>
2022-09-12 21:49:13 +1000

    Исправил до пункта 9

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
 
-
+//1. Создаём переменную с дирректорией, в которой будет осуществляться поиск.
 $searchRoot = './test_search';
+// Создаём переменную с именем файла, который необходимо найти в дирректории.
 $searchName = 'test.txt';
+//2. Создаём пустой массив, где в случае успеха будут храниться все совпадения с запросом.
 $searchResult = [];
-//Функция ,рекурсивно обходящая дирректории
+//3.Создаём функцию, рекурсивно обхлодящую дирректорию. В случае, нахождения файла, путь файла добавляется в массив.
 function search(string $dir, string $searchingFile, array &$resultArray)
 {
     foreach (scandir($dir) as $item) {
@@ -16,25 +18,37 @@ function search(string $dir, string $searchingFile, array &$resultArray)
     }
 }
 
-// Функция обёртка, позволяющая что либо делать, после обхода дирректорий и формирования массива результатов
-function searchFile(string $dir, string $searchingFile, array &$resultArray)
-{
-    search($dir, $searchingFile, $resultArray);
-    // функция, проверяющая пустой файл или нет
-    function isSize(string $file): bool
-    {
-        return (boolean)filesize($file);
-    }
-
-//Фильтруем массив от пустых файлов
-    $resultArray = array_filter($resultArray, 'isSize');
-    if (count($resultArray)) {
-        foreach ($resultArray as $item) {
-            echo $item . PHP_EOL;
-        }
-    } else {
-        echo 'Поиск не дал результатов!' . PHP_EOL;
-    }
+//4. Вызываем функцию с заданными аргументами
+search($searchRoot, $searchName, $searchResult);
+//5. Выводим результат, обходя массив. В случае отсутствия результата выводим соответствующее сообщение.
+if (count($searchResult) == 0) {
+    echo 'Поиск не дал результатов!';
+} else foreach ($searchResult as $item) {
+    echo $item . PHP_EOL;
 }
 
-searchFile($searchRoot, $searchName, $searchResult);
+
+
+
+// Функция обёртка, позволяющая что либо делать, после обхода дирректорий и формирования массива результатов
+//function searchFile(string $dir, string $searchingFile, array &$resultArray)
+//{
+//    search($dir, $searchingFile, $resultArray);
+//    // функция, проверяющая пустой файл или нет
+//    function isSize(string $file): bool
+//    {
+//        return (boolean)filesize($file);
+//    }
+//
+////Фильтруем массив от пустых файлов
+//    $resultArray = array_filter($resultArray, 'isSize');
+//    if (count($resultArray)) {
+//        foreach ($resultArray as $item) {
+//            echo $item . PHP_EOL;
+//        }
+//    } else {
+//        echo 'Поиск не дал результатов!' . PHP_EOL;
+//    }
+//}
+//
+//searchFile($searchRoot, $searchName, $searchResult);

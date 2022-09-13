177a60490a9ce465dbfed111afd8859899387825
Илья Соболев <123ilya@gmail.com>
2022-09-13 22:55:06 +1000

    исправил все

--- index.php
+++ index.php
@@ -9,25 +9,40 @@ $searchResult = [];
 //3.Создаём функцию, рекурсивно обхлодящую дирректорию. В случае, нахождения файла, путь файла добавляется в массив.
 function search(string $dir, string $searchingFile, array &$resultArray)
 {
+    //---------------------------------------------------------------------------
+//    foreach (scandir($dir) as $item) {
+//        if ($item !== '.' && $item !== '..' && is_dir($dir . '/' . $item)) {
+//            search($dir . '/' . $item, $searchingFile, $resultArray);
+//        } elseif ($item !== '.' && $item !== '..' && !is_dir($dir . '/' . $item) && $searchingFile == $item) {
+//            $resultArray[] = $dir . '/' . $item;
+//        }
+//    }
+    //-----------------------------------------------------------
     foreach (scandir($dir) as $item) {
-        if ($item !== '.' && $item !== '..' && is_dir($dir . '/' . $item)) {
+        if ($item == '.' || $item == '..') {
+            continue;
+        }
+        if (is_dir($dir . '/' . $item)) {
             search($dir . '/' . $item, $searchingFile, $resultArray);
-        } elseif ($item !== '.' && $item !== '..' && !is_dir($dir . '/' . $item) && $searchingFile == $item) {
+        } elseif ($searchingFile == $item) {
             $resultArray[] = $dir . '/' . $item;
         }
     }
+    //----------------------------------------------------------------
 }
 
 //4. Вызываем функцию с заданными аргументами
 search($searchRoot, $searchName, $searchResult);
 //9. Отфильтровываем вайлы с нулевым размером
 $searchResult = array_filter($searchResult, function ($x) {
-   return (boolean) filesize($x);
+    return (boolean)filesize($x);
 });
 
 //5. Выводим результат, обходя массив. В случае отсутствия результата выводим соответствующее сообщение.
 if (count($searchResult) == 0) {
     echo 'Поиск не дал результатов!';
-} else foreach ($searchResult as $item) {
-    echo $item . PHP_EOL;
+} else {
+    foreach ($searchResult as $item) {
+        echo $item . PHP_EOL;
+    }
 }
